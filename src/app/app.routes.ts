cd5a23a13661cef73b830f53f1423a8a55b48937
mo-otch <mh.isv78@gmail.com>
2024-12-02 16:47:37 +0100

    debug route conflit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { EventsComponent } from './pages/events/events.component';
-<<<<<<< Updated upstream
-
-=======
 import { ForumComponent } from './pages/forum/forum.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
->>>>>>> Stashed changes
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'events', component: EventsComponent },
-<<<<<<< Updated upstream
-=======
     { path: 'forum', component: ForumComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'articles', component: ArticlesComponent },
->>>>>>> Stashed changes
+    { path: 'events', component: EventsComponent },
 ];
