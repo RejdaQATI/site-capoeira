40c4a988785cf127a193e77d60681dccb7bde0ec
Rejda <qati.rejda@gmail.com>
2024-12-02 16:27:54 +0100

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+<<<<<<< HEAD
     
 ];
 
+=======
+    { path: 'events', component: EventsComponent },
+];
+>>>>>>> 29780e3 (latest changes)
