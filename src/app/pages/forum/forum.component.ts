95c4e3984056ab26701658ac4be3a8aa6e640427
Rejda <qati.rejda@gmail.com>
2024-12-03 10:43:10 +0100

    debug conflict

--- src/app/pages/forum/forum.component.ts
+++ src/app/pages/forum/forum.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; 
+import { DiscussionService } from '../../services/discussion.service';
+import { Discussion } from '../../models/discussion.model';
 
 @Component({
   selector: 'app-forum',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './forum.component.html',
-  styleUrl: './forum.component.css'
+  styleUrls: ['./forum.component.css'],
 })
-export class ForumComponent {
+export class ForumComponent implements OnInit {
+  discussions: Discussion[] = [];
 
+  constructor(private discussionService: DiscussionService) {}
+
+  ngOnInit(): void {
+    this.discussionService.getDiscussions().subscribe((data) => {
+      this.discussions = data;
+    });
+  }
 }
