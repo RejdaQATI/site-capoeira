74a970d97a8d528efad775475a7a49593a4fe55d
mo-otch <mh.isv78@gmail.com>
2024-12-03 10:25:10 +0100

    leaflet library

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  ngAfterViewInit() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  }
 
 }
